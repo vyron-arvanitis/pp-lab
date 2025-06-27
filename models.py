faf2650e6c4addf01c49fc5f086b51174654160a
Angelo <yellouz@Angelos-MacBook-Pro-2.local>
2025-06-27 18:31:45 +0200

    Task 2 and 3

--- models.py
+++ models.py
@@ -32,7 +32,6 @@ class GCN(nn.Module):
     def forward(self, inputs, adjacency):
         return adjacency @ self.linear(inputs)
 
-
 def masked_average(batch, mask):
     batch = batch.masked_fill(mask[..., np.newaxis], 0)
     sizes = (~mask).sum(axis=1, keepdim=True)
@@ -85,6 +84,28 @@ class CombinedModel(nn.Module):
         x = torch.cat([feat, emb], -1)
         return self.deep_set(dict(feat=x), mask=mask)
 
+class CombinedModel_wGCN(nn.Module):
+    def __init__(self, num_feat=8, embed_dim=8, num_pdg_ids=len(PDG_MAPPING), units=32):
+        super().__init__()
+        self.embedding = nn.Embedding(num_pdg_ids + 1, embed_dim)
+        self.deep_set = DeepSet_GCN(num_features=num_feat + embed_dim, units=units)
+
+    def forward(self, inputs, mask=None):
+        pdg = inputs["pdg"]
+        feat = inputs["feat"]
+        emb = self.embedding(pdg)
+        x = torch.cat([feat, emb], -1)
+        return self.deep_set(dict(feat=x, adj=inputs["adj"]), mask=mask)
+
+class OutputLayer(nn.Module):
+    def __init__(self, num_inputs):
+        self.output_layer = nn.Sequential(
+            nn.Linear(num_inputs, 1)
+        )
+    
+    def forward(self):
+        return  self.output_layer
+
 
 class GraphNetwork(nn.Module):
     """
@@ -100,6 +121,40 @@ class GraphNetwork(nn.Module):
         adj = normalize_adjacency(adj)
         return self.gcn(feat, adj)
 
+class DeepSet_GCN(nn.Module):
+    def __init__(self, num_features=8, units=32):
+        super().__init__()
+        self.gcn = GCN(units, units)
+        self.per_item_mlp = nn.Sequential(
+            nn.Linear(num_features, units),
+            nn.ReLU()
+        )
+        self.global_mlp = nn.Sequential(
+            nn.Linear(units, units),
+            nn.ReLU(),
+            nn.Linear(units, 1)
+        )
+
+    def forward(self, inputs, mask=None):
+        feat= inputs["feat"]
+        adj = inputs["adj"]
+        adj = normalize_adjacency(adj)
+
+        x = self.per_item_mlp(feat) # batch x particles x units
+        x = self.gcn(x, adj) # batch x particles x units
+
+        if mask is not None:
+            x = masked_average(x, mask)
+        else:
+            x = x.mean(axis=-2)
+        x = self.global_mlp(x)
+        return x
+
+
+
+
+
+    
 
 def from_config(config):
     """
@@ -109,6 +164,8 @@ def from_config(config):
         "deepset": DeepSet,
         "deepset_combined": CombinedModel,
         "gcn": GraphNetwork,
+        "deepset_gcn": DeepSet_GCN,
+        "deepset_combined_wgcn": CombinedModel_wGCN,
     }
     config = config.copy()
     return models[config.pop("model_name")](**config)
