264824582af80041aea8a78ef90d8284365bd27a
Arvanitis.Vyron <arvanitis.vyron@physik.uni-muenchen.de>
2025-08-04 17:13:06 +0200

    fixed bug with normalized_cartesian

--- workbook.py
+++ workbook.py
@@ -19,7 +19,7 @@ from utils import (
 from models import from_config
 
 def main():
-    coordinates = "cylindrical"
+    coordinates = "cartesian"
 
     feature_columns_map = {
         "cartesian": ["prodTime", "x", "y", "z", "energy", "px", "py", "pz"],
@@ -34,7 +34,7 @@ def main():
     feature_columns = feature_columns_map.get(coordinates)
 
     config = {
-    "model_name": "deepset_combined",
+    "model_name": "deepset_combined_wgcn_normalized",
     "embed_dim": 8,          # add embedding size
     #"dropout_rate": 0.179,     # add dropout     30 was better
     # "num_heads": 4,          # add number of transformer heads
@@ -49,9 +49,9 @@ def main():
     print("Selected model: ", tag)
 
     # --- Configuration ---
-    dataset_path = "smartbkg_dataset_4k_training.parquet"
+    dataset_path = "smartbkg_dataset_4k.parquet"
     pdg_map_path = "pdg_mapping.json"
-    save_path = Path("saved_models/Coordinates_Investigation")
+    save_path = Path("models_fulllllllll")
     save_path.mkdir(exist_ok=True)
     model_path = save_path / tag
     model_path.mkdir(exist_ok=True)
